2ae4f2e2c06149eaae11b5e83d7816f1c901511f
周义竣 <zhouyijun@bwcj.com>
2024-06-02 20:33:02 +0800

    feat: esprima

--- ast/demo1.js
+++ ast/demo1.js
@@ -1,7 +1,13 @@
+const parser = require("@babel/parser");
 const babel = require("@babel/core");
 const fs = require("fs");
 const path = require("path");
 const sumCode = fs.readFileSync(path.join(process.cwd(), "ast/sum.js"), "utf8");
+const esprima = require("esprima");
 
-const ast = babel.parse(sumCode, { sourceType: "module" });
-console.log(ast.program.body);
+// const ast = parser.parse(sumCode, { tokens: true });
+const token = esprima.tokenize(sumCode);
+const ast = esprima.parse(sumCode);
+// console.log(token);
+// console.log(JSON.stringify(ast, null, 2));
+// console.log(ast.program.body);
