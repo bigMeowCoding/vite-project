08f0d0483efdcc015c4558b39eb0240c0c95ed3b
周义竣 <zhouyijun@bwcj.com>
2024-07-21 15:25:51 +0800

    feat: reset demo

--- test/components/test.js
+++ test/components/test.js
@@ -1,3 +1,2 @@
-import I18N from 'src/utils/I18N';
-export const name = I18N.chagee.zhouYiJun;
-export const work = I18N.chagee.qianDuanKaiFaGong;
+export const name = "周义竣";
+export const work = "前端开发工程师";
