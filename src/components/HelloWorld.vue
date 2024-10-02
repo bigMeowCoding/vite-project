a670216afb69fe1f7da1ebe2b0bd9fa5fdce31b2
周义竣 <zhouyijun@bwcj.com>
2024-10-02 18:50:05 +0800

    feat: rest helloworld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,4 @@
-<template>
-<h1>{{ undefined }}</h1><div class="card"><button type="button"></button><p><code>{{ t('key_0') HelloWorld.vue }}</code></p></div><p><a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a></p><p><a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a></p><p class="read-the-docs">{{ t('key_1') }}</p>
-</template>
-
-<script setup setup>
-
+<script setup>
 import { ref } from "vue";
 import request from "umi-request";
 import { name, work } from "./test.js";
@@ -20,13 +15,38 @@ const count = ref(0);
 // request.post('/api/mock/test',{data:{a:1}}).then((res) => {
 //   console.log(res);
 // });
-
 </script>
 
-<style scoped>
+<template>
+  {{ name }}
+  {{ work }}
+  <h1>{{ props.msg }}</h1>
 
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      编辑
+      <code>组件 HelloWorld.vue</code> 测试热更新
+    </p>
+  </div>
+
+  <p>
+    哈哈
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >官方文档
+  </p>
+  <p>
+    {{ t("5") }}
+
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    最好的idea
+  </p>
+  <p class="read-the-docs">点击</p>
+</template>
+
+<style scoped>
 .read-the-docs {
   color: #888;
 }
-
 </style>
