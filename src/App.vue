1f038cbd874c0f01c1679229628892054670c530
周义竣 <zhouyijun@bwcj.com>
2024-10-04 15:39:28 +0800

    chore: reset app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
-  <el-select placeholder="Select"
-    ><el-option :label="t('key_0')" value="zh"></el-option
-    ><el-option label="English" value="en"></el-option
-    ><HelloWorld :msg="t('key_1')"></HelloWorld
-  ></el-select>
+  <el-select
+    v-model="locale"
+    placeholder="Select"
+    @change="
+      (val) => {
+        setLanguage(val);
+      }
+    "
+    style="width: 240px"
+  >
+    <el-option label="中文" value="zh" />
+    <el-option label="English" value="en" />
+    <HelloWorld msg="你好" />
+  </el-select>
 </template>
-
-<script setup setup lang="ts">
+<script setup lang="ts">
 import { useI18n } from "vue-i18n";
 const { t, locale } = useI18n();
 import HelloWorld from "./components/HelloWorld.vue";
 // const { locale, setLanguage } = useStore();
+function setLanguage(val) {
+  console.log(val);
+  locale.value = val;
+}
 </script>
 
 <style scoped>
