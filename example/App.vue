5558ae352883f7d5f815687a031af8499026a653
周义竣 <zhouyijun@bwcj.com>
2024-10-31 18:04:24 +0800

    feat: 更新HelloWorld组件和App.vue模板
    修改了HelloWorld.vue的模板结构，移除了i18n相关的代码，并简化了内容。同时在App.vue中调整了el-select的选项和HelloWorld组件的msg属性值。

--- example/App.vue
+++ example/App.vue
@@ -1,63 +1,29 @@
+<script setup>
+import { useI18n } from "vue-i18n";
+const { t, locale } = useI18n();
+import HelloWorld from "./components/HelloWorld.vue";
+// const { locale, setLanguage } = useStore();
+function setLanguage(val) {
+  console.log(val);
+  locale.value = val;
+}
+</script>
+
 <template>
-<el-select placeholder="Select" v-model="locale" @change="
+  <el-select
+    v-model="locale"
+    placeholder="Select"
+    @change="
       (val) => {
         setLanguage(val);
       }
-    " :style="{ width: '240px' }"><el-option value="zh" :label="undefined"></el-option><el-option label="English" value="en"></el-option></el-select><HelloWorld :msg="undefined"></HelloWorld>
+    "
+    style="width: 240px"
+  >
+    <el-option label="中文" value="zh" />
+    <el-option label="English" value="en" />
+  </el-select>
+  <HelloWorld msg="你好"></HelloWorld>
 </template>
 
-<script setup lang="ts">
-import { useI18n } from "vue-i18n";
-const {
-  t,
-  locale
-} = useI18n();
-import HelloWorld from "./components/HelloWorld.vue";
-
-// 欢迎使用我们的多语言应用
-const welcomeMessage = t("key_2");
-console.log(welcomeMessage);
-
-// 语言选项
-const languageOptions = [{
-  label: t("key_3"),
-  value: "zh-CN"
-}, {
-  label: t("key_4"),
-  value: "zh-TW"
-}, {
-  label: "English",
-  value: "en"
-}];
-
-// 设置语言函数
-function setLanguage(val) {
-  console.log(`${t("key_5")}: ${val}`);
-  locale.value = val;
-}
-
-// 当前日期
-const currentDate = new Date().toLocaleDateString("zh-CN");
-console.log(`${t("key_6")} ${currentDate}`);
-
-// 问候语
-function getGreeting() {
-  const hour = new Date().getHours();
-  if (hour < 12) {
-    return t("key_7");
-  } else if (hour < 18) {
-    return t("key_8");
-  } else {
-    return t("key_9");
-  }
-}
-console.log(getGreeting());
-</script>
-
-<style scoped>
-
-.a {
-  color: red;
-}
-
-</style>
+<style scoped></style>
--- example/components/HelloWorld.vue
+++ example/components/HelloWorld.vue
@@ -1,18 +1,39 @@
 <template>
-{{ name }} {{ work }} <h1>{{ props.msg }}</h1><div class="card"><button type="button" @click="count++">count is {{ count }}</button><p>{{ t('key_10') }}<code>{{ t('key_11') + ' HelloWorld.vue' }}</code>{{ t('key_12') }}</p></div><p>{{ t('key_13') }}<a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>{{ t('key_14') }}</p><p>{{ t("5") }} <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>{{ t('key_15') + 'idea' }}</p><p class="read-the-docs">{{ t('key_16') }}</p>
-</template>
+  {{ name }}
+  {{ work }}
+  <h1>{{ msg }}</h1>
 
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      编辑
+      <code>组件 HelloWorld.vue</code> 测试热更新
+    </p>
+  </div>
+
+  <p>
+    哈哈
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >官方文档
+  </p>
+  <p>
+    安装
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank" class
+      >Volar</a
+    >
+    最好的idea
+  </p>
+  <p class="read-the-docs">点击</p>
+</template>
 <script setup>
 import { ref } from "vue";
 import request from "umi-request";
 import { name, work } from "./test.js";
-import { useI18n } from "vue-i18n";
-const {
-  t
-} = useI18n();
-const props = defineProps({
-  msg: String
+defineProps({
+  msg: String,
 });
+
 const count = ref(0);
 // request.post('/api/getUser',{data:{a:1}}).then((res) => {
 //   console.log(res);
@@ -23,11 +44,7 @@ const count = ref(0);
 </script>
 
 <style scoped>
-
-
 .read-the-docs {
   color: #888;
 }
-
-
 </style>
