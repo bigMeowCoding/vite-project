cc2f7720e5d92c40297e8f917a8c185749fffc0f
周义竣 <zhouyijun@bwcj.com>
2024-11-01 16:22:10 +0800

    feat: onattribue

--- src/transformer/extractAndReplaceChineseInVue.js
+++ src/transformer/extractAndReplaceChineseInVue.js
@@ -1,101 +1,125 @@
 const fs = require("fs");
-const { compileTemplate, parse } = require("@vue/compiler-sfc");
-const { NodeTypes } = require("@vue/compiler-core");
-const { replaceChineseInText } = require("../utils/replaceChineseInText");
-const { traverseAst } = require("./transformAst");
+const { parse } = require("@vue/compiler-sfc");
 const { generateVueFile } = require("./generateVueFile");
 const { replaceChineseInScript } = require("./replaceChineseInScript");
-const { generateTemplateFromAst } = require("./generateTemplateFromAst");
+const ejs = require("ejs");
+const htmlparser2 = require("htmlparser2");
 
 function extractAndReplaceChineseInVue(filePath) {
   try {
     const source = fs.readFileSync(filePath, "utf-8");
     const { descriptor } = parse(source);
+    let templateCode = "";
 
     if (descriptor.template) {
-      const { ast } = compileTemplate({
-        source: descriptor.template.content,
-        filename: filePath,
-        id: "template",
-      });
-
-      traverseAst(ast, processNode);
-
-      function processNode(node) {
-        if (node.type === NodeTypes.ELEMENT) {
-          processElementNode(node);
-        } else if (node.type === NodeTypes.TEXT) {
-          processTextNode(node);
-        } else if (node.type === NodeTypes.TEXT_CALL) {
-          processTextCallNode(node);
-        } else if (node.type === NodeTypes.COMPOUND_EXPRESSION) {
-          processCompoundExpressionNode(node);
-        }
-      }
-
-      function processCompoundExpressionNode(node) {
-        if (Array.isArray(node.children)) {
-          processNode(node.children);
-        }
-      }
-
-      function processElementNode(node) {
-        if (Array.isArray(node.props)) {
-          node.props.forEach(processNodeProp);
-        }
-      }
-
-      function processNodeProp(prop) {
-        if (prop.type === NodeTypes.ATTRIBUTE && prop.value) {
-          const value = prop.value.content;
-          if (typeof value === "string" && /[\u4e00-\u9fff]/.test(value)) {
-            prop.value.content = replaceChineseInText(value);
-
-            if (prop.name !== "v-bind" && prop.name[0] !== ":") {
-              prop.name = ":" + prop.name;
-            }
-          }
-        }
-      }
-
-      function processTextNode(node) {
-        const text = node.content.trim();
-        if (text && /[\u4e00-\u9fff]/.test(text)) {
-          node.content = `{{ ${replaceChineseInText(text)} }}`;
-        }
-      }
-
-      function processTextCallNode(node) {
-        if (Array.isArray(node.content)) {
-          traverseAst(node.content, processNode);
-        } else {
-          processNode(node.content);
-        }
-      }
-
-      descriptor.template.content = generateTemplateFromAst(ast);
+      templateCode = generationSource(descriptor.template, templateHandle);
     }
 
-    // 处理 script 和 scriptSetup 部分
-    if (descriptor.script) {
-      descriptor.script.content = replaceChineseInScript(
-        descriptor.script.content,
-      );
-    }
-    if (descriptor.scriptSetup) {
-      descriptor.scriptSetup.content = replaceChineseInScript(
-        descriptor.scriptSetup.content,
-      );
-    }
+    console.log("templateCode", templateCode);
 
-    // 重新生成 Vue 文件内容
-    const generated = generateVueFile(descriptor);
-    fs.writeFileSync(filePath, generated, "utf-8");
-    console.log(`文件 ${filePath} 已更新`);
+    // // 处理 script 和 scriptSetup 部分
+    // if (descriptor.script) {
+    //   descriptor.script.content = replaceChineseInScript(
+    //     descriptor.script.content,
+    //   );
+    // }
+    // if (descriptor.scriptSetup) {
+    //   descriptor.scriptSetup.content = replaceChineseInScript(
+    //     descriptor.scriptSetup.content,
+    //   );
+    // }
+    //
+    // // 重新生成 Vue 文件内容
+    // const generated = generateVueFile(descriptor);
+    // fs.writeFileSync(filePath, generated, "utf-8");
+    // console.log(`文件 ${filePath} 已更新`);
   } catch (error) {
     console.error(`处理文件 ${filePath} 时出错:`, error);
     throw error;
   }
+}
+function templateHandle(code) {
+  let htmlString = "";
+  let attrsCache = {};
+  const parser = new htmlparser2.Parser(
+    {
+      onopentag(name) {
+        console.log("opentag", name);
+      },
+      onattribute(name, value, quote) {
+        console.log("onatrribute", name, value);
+        if (value) {
+          attrsCache[name] = value;
+        } else {
+          if (quote === undefined) {
+            attrsCache[name] = undefined;
+          } else {
+            attrsCache[name] = value;
+          }
+        }
+      },
+      ontext(text) {
+        htmlString += text;
+        console.log("text", text);
+      },
+      onclosetag(name, isImplied) {
+        console.log("closetag=====", name, isImplied);
+        if (isImplied) {
+          return;
+        }
+      },
+      oncomment(text, isImplied) {
+        console.log("comment", text, isImplied);
+      },
+    },
+    {
+      lowerCaseTags: false,
+      recognizeSelfClosing: true,
+      lowerCaseAttributeNames: false,
+      decodeEntities: false,
+    },
+  );
+  parser.write(code);
+  parser.end();
+  return htmlString;
+}
+
+function getWrapperTemplate(sfc) {
+  const { type, lang, attrs } = sfc;
+  let template = `<${type}`;
+  if (lang) {
+    template += ` lang="${lang}"`;
+  }
+  if (sfc.setup) {
+    template += ` setup`;
+  }
+  if (sfc.scoped) {
+    template += ` scoped`;
+  }
+  for (const attr in attrs) {
+    if (!["lang", "scoped", "setup"].includes(attr)) {
+      if (attrs[attr] === true) {
+        template += attr;
+      } else {
+        template += ` ${attr}="${attrs[attr]}"`;
+      }
+    }
+  }
+  template += `><%- code %></${type}>`;
+  return template;
+}
+
+function generationSource(sfc, handle) {
+  const wrapperTemplate = getWrapperTemplate(sfc);
+  let source;
+  try {
+    source = handle(sfc.content);
+  } catch (error) {
+    source = sfc.content;
+  }
+  return ejs.render(wrapperTemplate, {
+    code: source,
+  });
 }
 
 module.exports = { extractAndReplaceChineseInVue };
