75e854b13d221303e88c94db9b34d0b654e4121b
周义竣 <zhouyijun@bwcj.com>
2024-11-02 22:40:36 +0800

    feat： 更新HelloWorld.vue模板内容
    将HelloWorld.vue组件的模板内容简化为仅包含“中文”文本，移除了原有的多个元素和链接。

--- example/components/HelloWorld.vue
+++ example/components/HelloWorld.vue
@@ -1,30 +1,5 @@
 <template>
-  {{ name }}
-  {{ work }}
-  <h1>{{ msg }}</h1>
-  <!--woca1-->
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      编辑
-      <code>组件 HelloWorld.vue</code> 测试热更新
-    </p>
-  </div>
-
-  <p>
-    哈哈
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >官方文档
-  </p>
-  <p>
-    安装
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" class
-      >Volar</a
-    >
-    最好的idea
-  </p>
-  <p class="read-the-docs">点击</p>
+中文
 </template>
 <script setup>
 import { ref } from "vue";
