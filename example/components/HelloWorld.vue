5b914f5e80d6afcb484a7a845eecc97b971e52f9
周义竣 <zhouyijun@bwcj.com>
2024-11-27 23:30:12 +0800

    style: 删除多余代码

--- example/components/HelloWorld.vue
+++ example/components/HelloWorld.vue
@@ -3,8 +3,6 @@
 </template>
 <script setup>
 import { ref } from "vue";
-import request from "umi-request";
-import { name, work } from "./test.js";
 import { useI18n } from "vue-i18n";
 defineProps({
   msg: String,
@@ -12,12 +10,6 @@ defineProps({
 const { t } = useI18n();
 
 const count = ref(0);
-// request.post('/api/getUser',{data:{a:1}}).then((res) => {
-//   console.log(res);
-// });
-// request.post('/api/mock/test',{data:{a:1}}).then((res) => {
-//   console.log(res);
-// });
 </script>
 
 <style scoped>
