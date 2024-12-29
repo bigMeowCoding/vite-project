defad8b991f943bcd341d3f2427766c00ba233a3
周义竣 <zhouyijun@bwcj.com>
2024-12-29 18:32:22 +0800

    feat: clean

--- components.d.ts
+++ components.d.ts
@@ -10,5 +10,7 @@ declare module 'vue' {
     ElOption: typeof import('element-plus/es')['ElOption']
     ElSelect: typeof import('element-plus/es')['ElSelect']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
   }
 }
