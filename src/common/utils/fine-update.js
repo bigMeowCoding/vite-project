8918d0574f3f1840c14dff84ad14924b44f746d1
周义竣 <zhouyijun@chagee.com>
2025-12-28 11:30:28 +0800

    feat(utils): 添加fine-update.js实现基础React Hooks功能
    实现useEffect、useState和useMemo等基础React Hooks功能

---
+++ src/common/utils/fine-update.js
@@ -0,0 +1,46 @@
+const effectStack = [];
+export function useEffect(callback) {
+  const execute = () => {
+    cleanup(effect);
+    try {
+      effectStack.push(effect);
+      callback();
+    } finally {
+      effectStack.pop();
+    }
+  };
+
+  const effect = {
+    execute,
+    deps: new Set(),
+  };
+  execute();
+}
+
+function cleanup(effect) {
+  effect.deps.forEach((dep) => {
+    dep.delete(effect);
+  });
+  effect.deps.clear();
+}
+
+export function useState(value) {
+  const subs = new Set();
+  const getValue = () => {
+    const effect = effectStack[effectStack.length - 1];
+    effect && subscribe(effect, subs);
+    return value;
+  };
+  const setValue = (newValue) => {
+    value = newValue;
+    subs.forEach((sub) => sub.execute());
+  };
+  return [getValue, setValue];
+}
+
+function subscribe(effect, subs) {
+  subs.add(effect);
+  effect.deps.add(subs);
+}
+
+export function useMemo() {}
