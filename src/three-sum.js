5fc1eec3196881d0e390781b62ffb36e691b6890
周义竣 <zhouyijun@chagee.com>
2026-01-11 18:04:36 +0800

    refactor: 清理无用代码并优化项目配置
    删除不再使用的 trap-dp 相关文件
    修复 index.html 的格式和内容
    更新 vite.config.js 的格式
    重构 three-sum.js 的实现逻辑

--- src/three-sum.js
+++ src/three-sum.js
@@ -1,23 +1,38 @@
+
+
 /**
  * @param {number[]} nums
- * @return {number}
+ * @return {number[][]}
  */
-var longestConsecutive = function(nums) {
-    if(!nums.length) {
-        return 0
-    }
-    let set = new Set(nums);
-    let maxLen=0;
-    for(let num of set) {
-        if(!set.has(num -1)) {
-            let count =1;
-            let numsItem =num+1
-            while(set.has(numsItem)) {
-                count++
-                numsItem=numsItem+1;
+export var threeSum = function (nums) {
+    const result = [];
+    const len = nums.length;
+    if (len < 3) return result;
+
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < len - 2; i++) {
+        if (nums[i] > 0) break; // 如果当前数字大于 0，则三数之和一定大于 0
+        if (i > 0 && nums[i] === nums[i - 1]) continue; // 去重
+
+        let left = i + 1;
+        let right = len - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+                while (left < right && nums[left] === nums[left + 1]) left++; // 去重
+                while (left < right && nums[right] === nums[right - 1]) right--; // 去重
+                left++;
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
             }
-            maxLen=Math.max(count,maxLen);
         }
     }
-    return maxLen;
+    return result;
 };
