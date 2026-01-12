e7ba087ee2fe8487fd961259525d87e74c9540e2
周义竣 <zhouyijun@chagee.com>
2026-01-12 19:01:17 +0800

    feat: 实现快速选择算法查找第k大元素

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,31 @@
-import { ref } from "vue";
-import { defineStore } from "pinia";
-
-// export const useStore = defineStore("counter", () => {
-//   const local = ref("en");
-//   function setLanguage(lang) {
-//     local.value = lang;
-//   }
-//
-//   return { local, setLanguage };
-// });
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums, k) {
+  const kLargetIndex = nums.length - k;
+  let l = 0,
+    r = nums.length - 1;
+  while (l <= r) {
+    const pivotIndex = l + Math.floor(Math.random() * (r - l + 1));
+    [nums[pivotIndex], nums[r]] = [nums[r], nums[pivotIndex]];
+    const pivot = nums[r];
+    let scrollIndex = l;
+    for (let i = l; i < r; i++) {
+      if (nums[i] < pivot) {
+        [nums[i], nums[scrollIndex]] = [nums[scrollIndex], nums[i]];
+        scrollIndex++;
+      }
+    }
+    [nums[scrollIndex], nums[r]] = [nums[r], nums[scrollIndex]];
+    if (scrollIndex === kLargetIndex) {
+      return nums[scrollIndex];
+    } else if (scrollIndex < kLargetIndex) {
+      l = scrollIndex + 1;
+    } else {
+      r = scrollIndex - 1;
+    }
+  }
+  return nums[kLargetIndex];
+};
