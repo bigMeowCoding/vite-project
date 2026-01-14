e46551fa1a573457afb4900ad89c8acb1512a845
周义竣 <zhouyijun@chagee.com>
2026-01-14 12:26:38 +0800

    refactor(findk): 优化快速选择算法实现并修复变量命名
    改进随机枢轴选择逻辑，修复变量名拼写错误
    清理注释掉的代码，简化算法实现

--- src/store/findk.js
+++ src/store/findk.js
@@ -4,40 +4,35 @@
  * @return {number}
  */
 var findKthLargest = function (nums, k) {
-  const kLargetNumIndex = nums.length - k;
-//   function partion(l,r) {
-      let l = 0,
+  const kLargetIndex = nums.length - k;
+  let l = 0,
     r = nums.length - 1;
-    while(l<=r) {
-        let lt =l;
-        let gt = r;
-        let i = l;
-        const pivotIndex = Math.floor(Math.random()*(r-l-1))+l;
-        const pivot = nums[pivotIndex]
-        while(i<=gt) {
-            if(nums[i]<pivot){
-                [nums[i],nums[lt]]=[nums[lt],nums[i]];
-                                i++;
-                lt++;
-            } else if(nums[i]>pivot) {
-                [nums[i],nums[gt]]=[nums[gt],nums[i]];
-                
-                gt--;
-            } else {
-                i++;
-            }
-        }
-        
-        if(gt>=kLargetNumIndex && kLargetNumIndex>=lt){
-            return nums[kLargetNumIndex]
-        } else if(kLargetNumIndex<lt) {
-            r=lt-1
-        } else {
-            l=gt+1
-        }
+  while (l <= r) {
+    const pivotIndex = l + Math.floor(Math.random() * (r - l + 1));
+    const pivot = nums[pivotIndex];
+    let lt = l;
+    let gt = r;
+    let i = l;
+    while (i <= gt) {
+      if (nums[i] < pivot) {
+        [nums[i], nums[lt]] = [nums[lt], nums[i]];
+        lt++;
+        i++;
+      } else if (nums[i] > pivot) {
+        [nums[i], nums[gt]] = [nums[gt], nums[i]];
+        gt--;
+      } else {
+        i++;
+      }
     }
-//   }
-//   partion(0,nums.length -1)
-//   console.log(nums[kLargetNumIndex])
-  return nums[kLargetNumIndex]
+
+    if (kLargetIndex >= lt && kLargetIndex <= gt) {
+      return nums[kLargetIndex];
+    } else if (lt > kLargetIndex) {
+      r = lt - 1;
+    } else {
+      l = gt + 1;
+    }
+  }
+  //   return nums[kLargetIndex];
 };
